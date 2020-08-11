6467f2025cc733837baadb26dcd584eeec772c11
tawnynichols <65970072+tawnynichols@users.noreply.github.com>
2020-08-11 10:53:33 -0700

    Create schema

--- schema.sql
+++ schema.sql
@@ -1,33 +1,6 @@
 CREATE TABLE departments (
     dept_no VARCHAR PRIMARY KEY,
-    dept_name VARCHAR
-);
-
-CREATE TABLE dept_emp (
-    emp_no INT NOT NULL,
-    dept_no VARCHAR NOT NULL
-);
-
-CREATE TABLE dept_manager (
-    dept_no VARCHAR NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-    emp_no INT NOT NULL
-);
-
-CREATE TABLE employees (
-    emp_no INT PRIMARY KEY,
-    emp_title_id VARCHAR,
-	birth_date date,
-	first_name VARCHAR(30),
-	last_name VARCHAR(30),
-	sex VARCHAR(1),
-	hire_date date
-);
-
-CREATE TABLE salaries (
-    emp_no INT,
-    salary INT,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+    dept_name VARCHAR(30)
 );
 
 CREATE TABLE titles (
@@ -35,17 +8,40 @@ CREATE TABLE titles (
     title VARCHAR(30)
 );
 
-ALTER TABLE employees
-ADD FOREIGN KEY (emp_title_id) REFERENCES titles(title_id);
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY,
+    emp_title_id VARCHAR NOT NULL,
+	birth_date date,
+	first_name VARCHAR(30),
+	last_name VARCHAR(30),
+	sex VARCHAR(1),
+	hire_date date,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)	
+);
 
-ALTER TABLE dept_emp
-ADD PRIMARY KEY (emp_no, dept_no);
+CREATE TABLE dept_emp (
+    emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    dept_no VARCHAR NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
 
 
-SELECT * FROM departments
-SELECT * FROM dept_emp
-SELECT * FROM dept_manager
-SELECT * FROM employees
-SELECT * FROM salaries
-SELECT * FROM titles
+CREATE TABLE dept_manager (
+    dept_no VARCHAR NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (dept_no, emp_no)
+);
+
+CREATE TABLE salaries (
+    emp_no INT PRIMARY KEY,
+    salary INT,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+
+
 
