b7aacee1c9814c4bb1916d40d9a1599dd2f1da89
tawnynichols <65970072+tawnynichols@users.noreply.github.com>
2020-08-11 13:52:07 -0700

    Updating Queries

--- Data/Queries.sql
+++ Data/Queries.sql
@@ -10,6 +10,12 @@ SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees e
 LEFT JOIN salaries s ON e.emp_no = s.emp_no
 
+SELECT t.title, s.salary
+FROM employees e
+JOIN salaries s ON e.emp_no = s.emp_no
+JOIN titles t ON e.emp_title_id = t.title_id
+WHERE e.emp_no = 499942
+
 
 --List first name, last name, and hire date for employees who were hired in 1986.
 SELECT first_name, last_name, hire_date
